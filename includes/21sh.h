6df665d9117c30bbe4e88ffff26c2c08299010c9
Theophile DELMAS <thdelmas@e1r11p12.42.fr>
2019-05-12 22:10:53 +0200

    FIX 21sh.h

--- includes/21sh.h
+++ includes/21sh.h
@@ -6,23 +6,14 @@
 /*   By: maboye <maboye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 15:48:58 by thdelmas          #+#    #+#             */
-/*   Updated: 2019/05/12 22:07:02 by thdelmas         ###   ########.fr       */
+/*   Updated: 2019/05/12 22:10:35 by thdelmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SH21_H
 # define SH21_H
 
-
 # include "sh_line.h"
-
-# define RED		"\033[31m"
-# define GREEN		"\033[32m"
-# define YELLOW		"\033[33m"
-# define BLUE		"\033[34m"
-# define PURPLE		"\033[35m"
-# define TUR		"\033[36m"
-# define X			"\033[0m"
 
 typedef struct		s_sh
 {
