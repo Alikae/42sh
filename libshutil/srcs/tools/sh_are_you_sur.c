3c231bdf3f9b27c177dd1b4e2c8f5fae99835243
Etienne DE RAM <ede-ram@e1r8p9.42.fr>
2019-07-11 02:53:46 +0200

    MAJ adapt tokenizer + executer

--- libshutil/srcs/tools/sh_are_you_sur.c
+++ libshutil/srcs/tools/sh_are_you_sur.c
@@ -1,3 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_are_you_sur.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-ram <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 02:45:55 by ede-ram           #+#    #+#             */
+/*   Updated: 2019/07/11 02:47:14 by ede-ram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#define BUFFER_SIZE 4096
+
 int		sh_aree_you_sure(char *question, char *name)
 {
 	char	buff[BUFFER_SIZE];
@@ -9,7 +25,7 @@ int		sh_aree_you_sure(char *question, char *name)
 	ft_putstr_fd(name, 2);
 	while (ft_strchr(tmp, '\n'))
 	{
-		ret = read(0, buff, BUFFER_SIZE)
+		ret = read(0, buff, BUFFER_SIZE);
 		buff[ret] = '\0';
 		tmp = ft_strjoin(tmp, buff);
 	}
--- libshutil/srcs/tools/sh_check_path.c
+++ libshutil/srcs/tools/sh_check_path.c
@@ -1,4 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_check_path.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-ram <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 02:47:34 by ede-ram           #+#    #+#             */
+/*   Updated: 2019/07/11 02:49:34 by ede-ram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libshutil.h"
+#include "libft.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int		sh_find_error(char *path, char c, int *error)
 {
@@ -8,17 +25,17 @@ int		sh_find_error(char *path, char c, int *error)
 	mode = F_OK | X_OK;
 	if (stat(path, &s) == -1)
 	{
-		*error = NOT_FOUND;
+		*error = ENOENT;
 		return (0);
 	}
 	if (c != '\0' && (s.st_mode & S_IFDIR) != S_IFDIR)
 	{
-		*error = NOT_DIR;
+		*error = ENOTDIR;
 		return (0);
 	}
 	if (c != '\0' && access(path, mode) == -1)
 	{
-		*error = NOT_ACCESS;
+		*error = EACCES;
 		return (0);
 	}
 	return (1);
