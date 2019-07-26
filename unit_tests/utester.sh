52eef99aadaeea452f4854beeba4dd86abb9fc19
Theophile DELMAS <thdelmas@e1r5p2.42.fr>
2019-07-26 23:47:30 +0200

    MAJ | utester.sh | Minishell mode vs script mode

--- unit_tests/utester.sh
+++ unit_tests/utester.sh
@@ -7,6 +7,7 @@ ptofail=./pipefail
 pref=./piperef
 shtofail=./42sh
 shref=/bin/sh
+test_script=
 
 cmd=(\
 	'foo'\
@@ -27,12 +28,6 @@ cmd=(\
 	'exit'\
 	)
 
-mkfifo $logref $logfail $ptofail $pref
-
-## Tests
-$shtofail <$ptofail >>$logfail 2>&1 &
-$shref <$pref >>$logref 2>&1 &
-
 testy () {
 	echo "echo '\n''UT: $shtofail: $1'" > $ptofail
 	echo "$1" > $ptofail
@@ -40,12 +35,49 @@ testy () {
 	echo "$1" > $pref
 }
 
-for i in "${cmd[@]}"
-do
-testy "$i"
+test_minishell () {
+	mkfifo $logref $logfail $ptofail $pref
+
+	## Tests
+	$shtofail <$ptofail >>$logfail 2>&1 &
+	$shref <$pref >>$logref 2>&1 &
+
+	for i in "${cmd[@]}"
+	do
+		testy "$i"
+	done
+
+	diff $logref $logfail > $log_file
+	cat $log_file
+
+	rm -f $logref $logfail $pref $ptofail
+}
+
+test_script_sh () {
+	mkfifo $logref $logfail $ptofail $pref
+	if [ -z $test_script ] ; then
+		printf "Enter shell script's path: "
+		read test_script
+	fi
+	$shtofail $test_script >>$logfail 2>&1 &
+	$shref $test_script >>$logref 2>&1 &
+
+	diff $logref $logfail > $log_file
+	cat $log_file
+	rm -f $logref $logfail $pref $ptofail
+}
+
+while [ -z $user_in ] ; do
+	echo 'Test Minishell - 1'
+	echo 'Test Script Mode - 2'
+	printf "Choose testing mode: "
+	read user_in
+	clear
 done
-
-diff $logref $logfail > $log_file
-cat $log_file
-
-rm -f $logref $logfail $pref $ptofail
+if [ "$user_in" -eq "1" ] ; then
+	test_minishell
+elif [ "$user_in" -eq "2" ] ; then
+	test_script_sh
+else
+	echo 'bad input'
+fi
