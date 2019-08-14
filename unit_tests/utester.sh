0b5ffc0d604c3c5d141f9ed506191007b5125661
Theophile DELMAS <thdelmas@e1r13p8.42.fr>
2019-08-14 17:24:58 +0200

    MAJ unit_test

--- unit_tests/utester.sh
+++ unit_tests/utester.sh
@@ -6,7 +6,7 @@
 #    By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/28 08:54:49 by thdelmas          #+#    #+#              #
-#    Updated: 2019/08/14 17:17:15 by thdelmas         ###   ########.fr        #
+#    Updated: 2019/08/14 17:24:24 by thdelmas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/sh
@@ -14,7 +14,7 @@
 readonly UT_dir="./unit_tests"
 readonly UT_log_dir="$UT_dir/logs"
 readonly UT_log_file="./ulogs"
-readonly UT_sh_tgt="../msh/minishell"
+readonly UT_sh_tgt="./42sh"
 readonly UT_sh_ref="/bin/sh"
 
 #(I/)O REPORT FILES
