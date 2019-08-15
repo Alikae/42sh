4d4171df2d4b6bed57b61eecf949f357df54fd76
Theophile DELMAS <thdelmas@e1r8p14.42.fr>
2019-08-15 16:14:51 +0200

    MAJ unit_tests

--- unit_tests/shell_utests.sh
+++ unit_tests/shell_utests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cmd_file=$UT_log_dir/cmd_file
-cmd_file_base=$UT_log_dir/cmd_file_bae
+cmd_file_1=$UT_log_dir/cmd_file_1
+cmd_file_0=$UT_log_dir/cmd_file_0
 utests='
 pwd
 /usr/bin/env
@@ -36,7 +36,7 @@ ls
 ls /bin /boot /sbin /dev /home
 ls | cat -e
 ls | sort | cat -e
-base64 /dev/urandom | head -c 1000 | grep 42 | wc -l | sed -e 's/1/Yes/g' -e 's/0/No/g'
+base64 /dev/urandom | head -c 1000 | grep 42 | wc -l | sed -e s/1/Yes/g -e s/0/No/g
 echo "Testing redirections," > /tmp/test.txt ; cat /tmp/test.txt ; rm /tmp/test.txt ; ls /tmp
 echo "Testing redirections," > /tmp/test.txt ; cat /tmp/test.txt ; echo "Testing redirections," > /tmp/test.txt ; cat /tmp/test.txt ; rm /tmp/test.txt ; ls /tmp
 echo "Testing redirections," > /tmp/test.txt ; cat /tmp/test.txt ; echo "Testing redirections," > /tmp/test.txt ; cat /tmp/test.txt ; wc -c < /tmp/test.txt ; rm /tmp/test.txt ; ls /tmp
@@ -70,6 +70,7 @@ cd /path/to/dir
 mkdir test ; cd test ; ls -a ; ls | cat | wc -c > fifi ; cat fifi
 mkdir test ; cd test ; ls -a ; ls | cat | wc -c > fifi ; cat fifi; ls; rm -rf test; ls
 exit'
+
 ret1=
 ret2=
 UT_test_ok="0"
@@ -77,82 +78,42 @@ UT_test_num="0"
 
 shell_utests_stdin ()
 {
-	echo "\n\033[0;36;40m[ Testing: Stdin mode ] \033[0;32;40m[ OK ]\033[0;0m"
-	echo "\n\033[0;36;40m[ Testing: Stdin mode ] \033[0;32;40m[ OK ]\033[0;0m" >> $UT_res_file
-	while read i
-	do
-		echo "$i" | $UT_sh_tgt >$UT_out_file_tgt 2>$UT_err_file_tgt
-		echo "$i" | $UT_sh_ref >$UT_out_file_ref 2>$UT_err_file_ref
-		diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
-		ret1=$?
-		diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
-		ret2=$?
-		if [ -z "$(cat $UT_out_file_diff)" ]
-		then
-			print_kook $ret1 $ret2 "$i"
-		else
-			print_kook $ret1 $ret2 "$i"
-		fi
-		if [ $ret1 -eq '0' ]
-		then
-			UT_test_ok="$( expr "$UT_test_ok" + 1 )"
-		fi
-		UT_test_num="$( expr "$UT_test_num" + 1 )"
-	done <<< "$(cat $cmd_file)"
+	printf "\033[0;36;40m[ Stdin: ] \033[0;0m"
+	printf "\033[0;36;40m[ Stdin: ] \033[0;0m" >> $UT_res_file
+	echo "$1" | $UT_sh_tgt >$UT_out_file_tgt 2>$UT_err_file_tgt
+	echo "$1" | $UT_sh_ref >$UT_out_file_ref 2>$UT_err_file_ref
+	diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
+	ret1=$?
+	diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
+	ret2=$?
+	print_kook $ret1 $ret2 "$1"
 }
 
 shell_utests_file ()
 {
-	echo "\n\033[0;36;40m[ Testing: File mode ] \033[0;32;40m[ OK ]\033[0;0m"
-	echo "\n\033[0;36;40m[ Testing: File mode ] \033[0;32;40m[ OK ]\033[0;0m" >> $UT_res_file
-	while read i
-	do
-		echo "$i" > $test_file
-		$UT_sh_tgt $test_file >$UT_out_file_tgt 2>$UT_err_file_tgt
-		$UT_sh_ref $test_file >$UT_out_file_ref 2>$UT_err_file_ref
-		diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
-		ret1=$?
-		diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
-		ret2=$?
-		if [ -z "$(cat $UT_out_file_diff)" ]
-		then
-			print_kook $ret1 $ret2 "$i"
-		else
-			print_kook $ret1 $ret2 "$i"
-		fi
-		if [ $ret1 -eq '0' ]
-		then
-			UT_test_ok="$( expr "$UT_test_ok" + 1 )"
-		fi
-		UT_test_num="$( expr "$UT_test_num" + 1 )"
-	done <<< "$(cat $cmd_file)"
-	rm $test_file 2> $UT_log_file
+	printf "\033[0;36;40m[ File: ] \033[0;0m"
+	printf "\033[0;36;40m[ File: ] \033[0;0m" >> $UT_res_file
+	echo "$1" > $test_file
+	$UT_sh_tgt $test_file >$UT_out_file_tgt 2>$UT_err_file_tgt
+	$UT_sh_ref $test_file >$UT_out_file_ref 2>$UT_err_file_ref
+	diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
+	ret1=$?
+	diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
+	ret2=$?
+	print_kook $ret1 $ret2 "$1"
 }
 
 shell_utests_arg ()
 {
-	echo "\n\033[0;36;40m[ Testing: -c mode ] \033[0;32;40m[ OK ]\033[0;0m"
-	echo "\n\033[0;36;40m[ Testing: -c mode ] \033[0;32;40m[ OK ]\033[0;0m" >> $UT_res_file
-	while read i
-	do
-		$UT_sh_tgt -c "$i" >$UT_out_file_tgt 2>$UT_err_file_tgt
-		$UT_sh_ref -c "$i" >$UT_out_file_ref 2>$UT_err_file_ref
-		diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
-		ret1=$?
-		diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
-		ret2=$?
-		if [ -z "$(cat $UT_out_file_diff)" ]
-		then
-			print_kook $ret1 $ret2 "$i"
-		else
-			print_kook $ret1 $ret2 "$i"
-		fi
-		if [ $ret1 -eq '0' ]
-		then
-			UT_test_ok="$( expr "$UT_test_ok" + 1 )"
-		fi
-		UT_test_num="$( expr "$UT_test_num" + 1 )"
-	done <<< "$(cat $cmd_file)"
+	printf "\033[0;36;40m[ -c: ] \033[0;0m"
+	printf "\033[0;36;40m[ -c: ] \033[0;0m" >> $UT_res_file
+	$UT_sh_tgt -c "$1" >$UT_out_file_tgt 2>$UT_err_file_tgt
+	$UT_sh_ref -c "$1" >$UT_out_file_ref 2>$UT_err_file_ref
+	diff "$UT_out_file_tgt" "$UT_out_file_ref" > $UT_out_file_diff
+	ret1=$?
+	diff "$UT_err_file_tgt" "$UT_err_file_ref" > $UT_err_file_diff
+	ret2=$?
+	print_kook $ret1 $ret2 "$1"
 }
 
 shell_utests ()
@@ -167,8 +128,8 @@ shell_utests ()
 	ls
 	pwd'
 
-	echo "$utests_base" > $cmd_file_base
-	echo "$utests" > $cmd_file
+	echo "$utests_base" > $cmd_file_0
+	echo "$utests" > $cmd_file_1
 	for i in ${utests_base[@]}
 	do
 		if [ "$stdin" = 'false' ]
@@ -181,7 +142,6 @@ shell_utests ()
 			if [ "$ret1" != '' ] || [ "$ret2" != '' ]
 			then
 				stdin='true'
-				shell_utests_stdin
 			fi
 		fi
 
@@ -194,7 +154,6 @@ shell_utests ()
 			if [ "$ret1" != '' ] || [ "$ret2" != '' ]
 			then
 				arg='true'
-				shell_utests_arg
 			fi
 		fi
 
@@ -208,7 +167,6 @@ shell_utests ()
 			if [ "$ret1" != '' ] || [ "$ret2" != '' ]
 			then
 				script_file='true'
-				shell_utests_file
 			fi
 		fi
 
@@ -232,6 +190,27 @@ shell_utests ()
 		echo "\033[0;36;40m[ Testing: File ] \033[0;31;40m[ KO ]\033[0;0m"
 		echo "\033[0;36;40m[ Testing: File ] \033[0;31;40m[ KO ]\033[0;0m" >> $UT_res_file
 	fi
+	while read i
+	do
+		if [ "$arg" = 'true' ]
+		then
+			shell_utests_arg "$i"
+		fi
+		if [ "$script_file" = 'true' ]
+		then
+			shell_utests_file "$i"
+		fi
+		if [ "$stdin" = 'true' ]
+		then
+			shell_utests_stdin "$i"
+		fi
+		echo
+		if [ $ret1 -eq '0' ]
+		then
+			UT_test_ok="$( expr "$UT_test_ok" + 1 )"
+		fi
+		UT_test_num="$( expr "$UT_test_num" + 1 )"
+	done <$cmd_file_1
 	echo "\033[0;36;40m[ $UT_sh_tgt ] [ Score: $UT_test_ok / $UT_test_num ]\033[0;0m"
 	echo "\033[0;36;40m[ $UT_sh_tgt ] [ Score: $UT_test_ok / $UT_test_num ]\033[0;0m" >> $UT_res_file
 	read -p "Display result ? (y/n): " ans
@@ -239,5 +218,5 @@ shell_utests ()
 	then
 		cat $UT_res_file
 	fi
-	rm $test_file $cmd_file $cmd_file_base 2>> "$UT_log_file"
+	rm $test_file $cmd_file_1 $cmd_file_0 2>> "$UT_log_file"
 }
--- unit_tests/utester_tools.sh
+++ unit_tests/utester_tools.sh
@@ -6,7 +6,7 @@
 #    By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/28 09:09:16 by thdelmas          #+#    #+#              #
-#    Updated: 2019/08/14 17:21:13 by thdelmas         ###   ########.fr        #
+#    Updated: 2019/08/15 16:07:24 by thdelmas         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/bin/sh
@@ -59,32 +59,29 @@ ask_for_out () {
 }
 
 print_kook () {
-	if [ "$1" = '0' ] && [ "$2" = '0' ]
+	if [ "$1" -eq '1' ] && [ "$2" -eq '1' ]
 	then
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m"
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m" >> $UT_res_file
-	fi
-	if [ "$1" != '0' ] && [ "$2" = '0' ]
-	then
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m"
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m\n\033[0;36;40m[ Stdout REPORT ]\033[0;0m" >> $UT_res_file
-		echo "<Your shell: $UT_sh_tgt\n>Ref shell: $UT_sh_ref" >> $UT_res_file
-		cat "$UT_out_file_diff" >> $UT_res_file
-	fi
-	if [ "$1" = '0' ] && [ "$2" != '0' ]
-	then
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m"
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m\n\033[0;36;40m[ Stderr REPORT ]\033[0;0m" >> $UT_res_file
-		echo "<Your shell: $UT_sh_tgt\n>Ref shell: $UT_sh_ref" >> $UT_res_file
-		cat "$UT_err_file_diff" >> $UT_res_file
-	fi
-	if [ "$1" != '0' ] && [ "$2" != '0' ]
-	then
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m"
-		echo "\033[0;36;40m[ Testing: $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m\n\033[0;36;40m[ Stdout REPORT ]\033[0;0m" >> $UT_res_file
+		echo "\033[0;36;40m[ $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m"
+		echo "\033[0;36;40m[ $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m\n\033[0;36;40m[ Stdout REPORT ]\033[0;0m" >> $UT_res_file
 		echo "<Your shell: $UT_sh_tgt\n>Ref shell: $UT_sh_ref" >> $UT_res_file
 		cat "$UT_out_file_diff" >> $UT_res_file
 		echo "\033[0;36;40m[ Stderr REPORT ]\033[0;0m" >> $UT_res_file
+		cat "$UT_err_file_diff" >> $UT_res_file
+	elif [ "$1" -eq '0' ] && [ "$2" -eq '0' ]
+	then
+		echo "\033[0;36;40m[ $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m"
+		echo "\033[0;36;40m[ $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m" >> $UT_res_file
+	elif [ "$1" -eq '0' ] && [ "$2" -eq '0' ]
+	then
+		echo "\033[0;36;40m[ $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m"
+		echo "\033[0;36;40m[ $3 ] \033[0;31;40m[ OUT: KO ]\033[0;0m \033[0;32;40m[ ERR: OK ]\033[0;0m\n\033[0;36;40m[ Stdout REPORT ]\033[0;0m" >> $UT_res_file
+		echo "<Your shell: $UT_sh_tgt\n>Ref shell: $UT_sh_ref" >> $UT_res_file
+		cat "$UT_out_file_diff" >> $UT_res_file
+	elif [ "$1" -eq '1' ] && [ "$2" -eq '1' ]
+	then
+		echo "\033[0;36;40m[ $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m"
+		echo "\033[0;36;40m[ $3 ] \033[0;32;40m[ OUT: OK ]\033[0;0m \033[0;31;40m[ ERR: KO ]\033[0;0m\n\033[0;36;40m[ Stderr REPORT ]\033[0;0m" >> $UT_res_file
+		echo "<Your shell: $UT_sh_tgt\n>Ref shell: $UT_sh_ref" >> $UT_res_file
 		cat "$UT_err_file_diff" >> $UT_res_file
 	fi
 }
