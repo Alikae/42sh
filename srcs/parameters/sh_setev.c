45a47ed664f58bacfc1e06879cb537fbfa67a37d
Theophile DELMAS <thdelmas@e1r9p8.42.fr>
2019-09-08 22:35:14 +0200

    EDT sh_setev

--- srcs/parameters/sh_setev.c
+++ srcs/parameters/sh_setev.c
@@ -6,7 +6,7 @@
 /*   By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 16:23:59 by thdelmas          #+#    #+#             */
-/*   Updated: 2019/09/08 06:36:35 by ede-ram          ###   ########.fr       */
+/*   Updated: 2019/09/08 22:34:04 by thdelmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,6 @@ void lsp(){}
 
 t_env	*sh_setev(const char *key, const char *value)
 {
-	//
-	printf("%s%s\n", key, value);
-	//
 	t_env	*env;
 
 	if (!key)
@@ -41,50 +38,13 @@ t_env	*sh_setev(const char *key, const char *value)
 		ft_putendl_fd(": readonly variable", 2);
 		return (env);
 	}
-	if (ft_strcmp(env->key, key))
+	else if (ft_strcmp(env->key, key))
 	{
 		env->next = sh_create_param(key);
 		env = env->next;
 	}
-	if (env->value)
+	else if (env->value)
 		free(env->value);
 	env->value = ft_strdup(value);
 	return (env);
-
-
-	//TO REDO
-	/*t_env	*env;
-
-	if (!key)
-		return (NULL);
-	if (!(env = sh()->params))
-		//DONT SET ENV BUT NEED IT NEVERTHELLESS
-		//RETURN GARBAGE
-		sh()->params = sh_create_param(key);
-	else if (ft_strcmp(env->key, key))
-	{
-		while (env->next && ft_strcmp(env->next->key, key))
-			env = env->next;
-	lsp();
-		if (!env->next)//WILL CREATE NEW IF SEARCHED ENV IS THE LAST
-			env->next = sh_create_param(key);
-		env = env->next;
-	}
-	if (value && env && env->readonly)
-	{
-		ft_putstr_fd(env->key, 2);
-		ft_putendl_fd(": readonly variable", 2);
-	}
-	else if (value && env)
-	{
-		if (env->value)
-			ft_strdel(&(env->value));
-		env->value = ft_strdup(value);
-	}
-	else if (env)
-	{
-		if (env->value)
-			ft_strdel(&(env->value));
-	}
-	return (env);*/
 }
