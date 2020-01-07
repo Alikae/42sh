9e986930472781f4d74b0327014c1ab8bb3de7f2
tcillard <tcillard@github.com>
2020-01-07 03:28:13 +0100

    add, positional parameters

--- srcs/word_expansion/sh_simple_word.c
+++ srcs/word_expansion/sh_simple_word.c
@@ -6,7 +6,7 @@
 /*   By: tcillard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 02:40:54 by tcillard          #+#    #+#             */
-/*   Updated: 2019/09/19 15:10:38 by tcillard         ###   ########.fr       */
+/*   Updated: 2020/01/07 03:27:03 by tcillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,26 @@
 
 void	sh_simple_expansion(t_exp *exp)
 {
+	int		i;
+	int		j;
+	char	**av;
+
+	i = 0;
+	j = 0;
+	av = NULL;
 	sh_record_name(exp);
 	if (exp->find)
 	{
 		exp->value = ft_strdup(exp->find->value);
 		sh_spetial_quote(&(exp->value));
+	}
+	else if (exp->name[0] >= '0' && exp->name[0] <= '9')
+	{
+		i = ft_atoi(exp->name);
+		av = sh()->av;
+		while (av[j] && j != i)
+			j++;
+		if (av[j])
+			exp->value = ft_strdup(av[j]);
 	}
 }
