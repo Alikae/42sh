09223ea8463aedf3091e7e60c1f04262f68d6bed
Jerry <jerry@dump-accueil-2.42.fr>
2020-01-09 15:55:31 +0100

    norm(sh_test.c sh_test_unary.c sh_cd.c): norme ok
    fixed bug on pwd value cd builtin

--- srcs/builtins/sh_cd.c
+++ srcs/builtins/sh_cd.c
@@ -6,7 +6,7 @@
 /*   By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/06 01:19:23 by thdelmas          #+#    #+#             */
-/*   Updated: 2020/01/09 11:40:52 by tmeyer           ###   ########.fr       */
+/*   Updated: 2020/01/09 13:01:03 by thdelmas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ static int	cd_go_to(char *path)
 		*tmp = '\0';
 	ft_bzero(dir, PATH_MAX + 1);
 	getcwd(dir, PATH_MAX);
+	path = sh_resolve_dotpath(path);
 	if (chdir(path))
 	{
 		ft_putstr_fd("cd: can't access: ", 2);
@@ -37,9 +38,11 @@ static int	cd_go_to(char *path)
 	}
 	if ((ev = sh_setev("OLDPWD", dir)))
 		ev->exported = 1;
+	ft_bzero(dir, PATH_MAX + 1);
 	getcwd(dir, PATH_MAX);
 	if ((ev = sh_setev("PWD", dir)))
 		ev->exported = 1;
+	ft_memdel((void**)&path);
 	return (0);
 }
 
--- srcs/sources.mk
+++ srcs/sources.mk
@@ -6,7 +6,7 @@
 #    By: thdelmas <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/13 19:40:02 by thdelmas          #+#    #+#              #
-#    Updated: 2020/01/09 09:52:24 by tmeyer           ###   ########.fr        #
+#    Updated: 2020/01/09 15:53:17 by jerry            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -137,6 +137,7 @@ C_FILES_builtins = \
 		   sh_unset.c \
 		   sh_set.c \
 		   sh_test.c \
+		   sh_test_unary.c \
 		   sh_source.c \
 		   sh_type.c \
 		   sh_true.c \
