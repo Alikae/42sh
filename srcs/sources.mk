6d5cc4a1190d3c90c5aace86e125375189633b37
Etienne DE RAM <ede-ram@e1r9p8.42.fr>
2020-02-04 01:54:03 +0100

    FIX norm

--- srcs/sources.mk
+++ srcs/sources.mk
@@ -6,7 +6,7 @@
 #    By: ede-ram <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/27 13:33:06 by ede-ram           #+#    #+#              #
-#    Updated: 2020/01/31 02:15:41 by tcillard         ###   ########.fr        #
+#    Updated: 2020/02/04 00:44:49 by ede-ram          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -129,6 +129,7 @@ C_FILES_executer = \
 	sh_exec_script_tools.c \
 	sh_exec_script_tools_2.c \
 	sh_exec_script_tools_3.c \
+	sh_exec_script_tools_4.c \
 	sh_redirection_tools.c
 
 C_FILES_builtins = \
