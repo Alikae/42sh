1fd087214254d53ac958e1cba7de65e30594bde8
tcillard <tcillard@github.com>
2020-02-10 04:33:13 +0100

    maj, subsh

--- srcs/word_expansion/sh_subsh_expansion.c
+++ srcs/word_expansion/sh_subsh_expansion.c
@@ -6,7 +6,7 @@
 /*   By: ede-ram <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/27 13:17:07 by ede-ram           #+#    #+#             */
-/*   Updated: 2020/01/27 13:17:09 by ede-ram          ###   ########.fr       */
+/*   Updated: 2020/02/10 04:32:26 by tcillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,8 @@ void	sh_read_pipe(t_exp *exp, int fd)
 		buff[ret] = '\0';
 		sh_subshdup(buff, exp);
 	}
+	if (exp->value[ft_strlen(exp->value) - 1] == '\n')
+		exp->value[ft_strlen(exp->value) - 1] = '\0';
 }
 
 void	sh_subsh_expansion(t_exp *exp)
