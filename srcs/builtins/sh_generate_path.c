67f878212e4ea57b55ff7a1b8ff3233bdecbe985
tcillard <tcillard@github.com>
2020-02-15 02:27:33 +0100

    maj, generate path

--- srcs/builtins/sh_generate_path.c
+++ srcs/builtins/sh_generate_path.c
@@ -6,7 +6,7 @@
 /*   By: tcillard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 23:05:28 by tcillard          #+#    #+#             */
-/*   Updated: 2020/02/15 01:01:44 by jerry            ###   ########.fr       */
+/*   Updated: 2020/02/15 02:27:20 by tcillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	sh_del_one_dir(char **new)
 		i--;
 	while (cpy[i] != '/')
 		i--;
-	if (!(*new = malloc(i)))
+	if (!(*new = malloc(i + 1)))
 		destructor(-1);
 	while (j < i)
 	{
@@ -101,7 +101,6 @@ void	sh_generate_path(char *path)
 	else
 		new = ft_strdup(cpy);
 	sh_find_new_path(path, &new);
-	sh_setev("PWD", new);
 	ft_memdel((void**)&cpy);
 	sh()->pwd = new;
 }
