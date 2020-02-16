63c2a16afc314a594569a810cddfa38f472ee90c
Thomas MEYER <T.Meyer.xp6@gmail.com>
2020-02-17 00:32:03 +0100

    FIX cd

--- srcs/builtins/sh_cd2.c
+++ srcs/builtins/sh_cd2.c
@@ -1,6 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sh_cd2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmeyer <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/17 00:31:32 by tmeyer            #+#    #+#             */
+/*   Updated: 2020/02/17 00:31:49 by tmeyer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "sh_tools.h"
 #include "sh_builtins.h"
 #include "libft.h"
+
+char			*check_av(char *av)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (!av && !(tmp = sh_getev_value("HOME")))
+	{
+		sh_dprintf(2, "NO HOME SET\n");
+		return (NULL);
+	}
+	else if (!strcmp(av, "-") && !(tmp = sh_getev_value("OLDPWD")))
+	{
+		sh_dprintf(2, "NO OLDPWD SET\n");
+		return (NULL);
+	}
+	return (av);
+}
 
 static char		**init_tab(void)
 {
@@ -14,9 +44,15 @@ static char		**init_tab(void)
 	return (cd_path);
 }
 
+static void		dell_everything(char **av, char ***path)
+{
+	ft_memdel((void**)av);
+	ft_tab_strdel(path);
+}
+
 char			*sh_try_cd_path(char *arg, char flag)
 {
-	int 	i;
+	int		i;
 	char	*av;
 	char	**cd_path;
 	char	**path;
@@ -36,9 +72,8 @@ char			*sh_try_cd_path(char *arg, char flag)
 			path = tab_realloc(path, av);
 		}
 		if ((real = path_process(av, path, flag)))
-			i = -2; 
-		ft_memdel((void**)&av);
-		ft_tab_strdel(&path);
+			i = -2;
+		dell_everything(&av, &path);
 	}
 	ft_tab_strdel(&cd_path);
 	return (real);
